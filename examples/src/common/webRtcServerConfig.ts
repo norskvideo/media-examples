65058f8857230fa00692cea34785bf694e7552d9
Adrian Roe <roe.adrian@gmail.com>
2023-08-28 18:54:56 +0100

    Change default webRTC server config to not change hostIp

--- examples/src/common/webRtcServerConfig.ts
+++ examples/src/common/webRtcServerConfig.ts
@@ -1,7 +1,6 @@
 import { WhepOutputSettings } from "@norskvideo/norsk-sdk";
 
-const hostIps = (process.env.CLIENT_HOST) ? { hostIps: [process.env.CLIENT_HOST] } : {}
-const turnServers =
+export const webRtcServerConfig: WhepOutputSettings =
     (process.env.TURN_INTERNAL && process.env.TURN_EXTERNAL) ?
         // Separate hostnames for server and client access to the turn server as in some cases they cannot resolve the same IP
         {
@@ -9,6 +8,3 @@ const turnServers =
             reportedIceServers: [{ urls: [`turn:${process.env.TURN_EXTERNAL}:3478`], username: "norsk", credential: "norsk" }],
         }
         : {}
-
-export const webRtcServerConfig: WhepOutputSettings =
-    { ...turnServers, ...hostIps };
